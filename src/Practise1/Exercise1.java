7e89c1acffc14592400370837f1ea6a76c9aa209
Andrey Liashenko <andreylachenko650@gmail.com>
2022-08-18 10:43:03 +0300

    test

--- src/Practise1/Exercise1.java
+++ src/Practise1/Exercise1.java
@@ -2,7 +2,7 @@ package Practise1;
 
 public class Exercise1 {
     public static void main(String[] args) {
-        String s = "ds";
+        String s = "Hello world";
         System.out.println(s);
     }
 }
